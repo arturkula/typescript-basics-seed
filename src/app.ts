d2bc157c6169db0d715fecd21b93a49bed1c48ae
Artur Kula <kontakt@arturkula.com>
2019-03-12 06:49:23 +0100

    finish lesson: "The "Any" Type"

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,4 @@
-const pizzas: number = 5;
-
-function offerDiscount(orders: number): boolean {
-    return orders >= 3;
-}
-
-if (offerDiscount(pizzas)) {
-    console.log(`You're entitled to a discount`);
-} else {
-    console.log(`Order more than 3 pizzas for a discount`);
-}
+let coupon: any;
+coupon = 25;
+coupon = '25';
+coupon = false;
