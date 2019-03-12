8604733d80059cb7d81911822a238adb02d9fee0
Artur Kula <kontakt@arturkula.com>
2019-03-12 07:07:07 +0100

    finish lesson: "Null, Undefined, Strict Null checks"

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 // run in console with:
 // clear; "running tsc:"; tsc; ""; "running node:"; ""; node .\dist\app.js; "";
 
-function orderError(error: string): never {
-    throw new Error(error);
-    // never going to return a value!
+let coupon: string | null = 'pizza25';
+
+function removeCoupon() {
+    coupon = null;
 }
 
-orderError('Something went wrong');
+console.debug(coupon);
+
+removeCoupon();
+
+console.debug(coupon);
