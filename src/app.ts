dfa6a3d97a6423a249d0cefc6f6ec0bcccdb7222
Artur Kula <kontakt@arturkula.com>
2019-03-12 06:58:06 +0100

    finish lesson: "Void Type"

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,12 @@
-let implicitCoupon = 'pizza25';
-let explicitCoupon: string;
-explicitCoupon = 'pizza25';
+// run in console with:
+// clear; "running tsc:"; tsc; ""; "running node:"; ""; node .\dist\app.js; "";
+
+let selectedTopping: string = 'pepperoni';
+
+function selectTopping(topping: string): void {
+    selectedTopping = topping;
+}
+
+selectTopping('bacon');
+
+console.debug(selectedTopping);
