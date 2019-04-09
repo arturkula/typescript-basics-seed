1aeb89b7dc5237611975adcfe5412d0d6364754c
Artur Kula <kontakt@arturkula.com>
2019-04-09 07:06:57 +0200

    finish lesson: "Functions and Optional Arguments"

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 // run in console with:
 // clear; "running tsc:"; tsc; ""; "running node:"; ""; node .\dist\app.js; "";
 
-let sumOrder: (price: number, quantity: number) => number;
+let sumOrder: (price: number, quantity?: number) => number;
 
-sumOrder = (x, y): number => x * y;
+sumOrder = (x, y): number => {
+    if (y) {
+        return x * y;
+    }
+    return x;
+};
 
-const sum = sumOrder(25, 2);
+const sum = sumOrder(25);
 
 console.debug(`total sum: ${sum}`);
