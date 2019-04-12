3505583e84e4bfef237699264492084b9c72dfef
Artur Kula <kontakt@arturkula.com>
2019-04-12 07:05:03 +0200

    finish lesson: "Setters and Getters (Accessors)"

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,25 @@
 // run in console with: clear; "running tsc:"; tsc; ""; "running node:"; ""; node .\dist\app.js; "";
 
+class Sizes {
+    constructor(public sizes: string[]) {}
+
+    set availableSizes(sizes: string[]) {
+        this.sizes = sizes;
+    }
+
+    get availableSizes(): string[] {
+        return this.sizes;
+    }
+}
+
+const x = new Sizes(['small', 'medium']);
+
+console.debug(x.availableSizes);
+
+x.availableSizes = ['medium', 'large'];
+
+console.debug(x.availableSizes);
+
 class Pizza {
     constructor(public readonly name: string, private readonly toppings: string[] = []) {}
 
@@ -12,4 +32,4 @@ const pizza = new Pizza('Pepperoni');
 
 pizza.addTopping('pepperoni');
 
-console.debug(pizza.name);
+// console.debug(pizza.name);
