de4016c722fb640d3e4a3e97a3bdddd3b68f5009
Artur Kula <kontakt@arturkula.com>
2019-04-12 06:55:05 +0200

    finish lesson: "Public and Private Members"

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,14 @@
 // run in console with: clear; "running tsc:"; tsc; ""; "running node:"; ""; node .\dist\app.js; "";
 
 class Pizza {
-    private name: string;
-    private toppings: string[] = [];
-
-    constructor(name: string) {
-        this.name = name;
-    }
+    constructor(private name: string, private toppings: string[] = []) {}
 
     public addTopping(topping: string) {
         this.toppings.push(topping);
     }
 }
 
-const pizza = new Pizza('pepperoni');
+const pizza = new Pizza('Pepperoni');
 
 pizza.addTopping('pepperoni');
 
