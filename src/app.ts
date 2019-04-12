5153a1bc855f7201c906b91d7f569eb0f12fd087
Artur Kula <kontakt@arturkula.com>
2019-04-12 07:26:53 +0200

    finish lesson: "Static Properties and Methods"

--- src/app.ts
+++ src/app.ts
@@ -1,49 +1,11 @@
 // run in console with: clear; "running tsc:"; tsc; ""; "running node:"; ""; node .\dist\app.js; "";
 
-interface ISizes {
-    availableSizes: string[];
-}
+class Coupon {
+    public static allowed = ['Pepperoni', 'Blazing Inferno'];
 
-abstract class Sizes implements ISizes {
-    constructor(protected sizes: string[]) {}
-
-    set availableSizes(sizes: string[]) {
-        this.sizes = sizes;
-    }
-
-    get availableSizes(): string[] {
-        return this.sizes;
+    public static create(percentage: number) {
+        return `PIZZA_RESTAURANT_${percentage}`;
     }
 }
 
-interface IPizza extends ISizes {
-    readonly name: string;
-    readonly toppings: string[];
-    updateSizes(sizes: string[]): void;
-    addTopping(topping: string): void;
-}
-
-// tslint:disable-next-line:max-classes-per-file
-class Pizza extends Sizes implements IPizza {
-    public readonly toppings: string[] = [];
-
-    constructor(public readonly name: string, sizes: string[] = []) {
-        super(sizes);
-    }
-
-    public updateSizes(sizes: string[]) {
-        this.sizes = sizes;
-    }
-
-    public addTopping(topping: string) {
-        this.toppings.push(topping);
-    }
-}
-
-const pizza = new Pizza('Pepperoni', ['small', 'medium']);
-
-console.debug(pizza.availableSizes);
-
-pizza.updateSizes(['large']);
-
-console.debug(pizza.availableSizes);
+console.debug(Coupon.create(25));
